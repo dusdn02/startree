6599591713ed9378aa77dd579f151b1f56fee74a
연우 이 <lyw9114@gmail.com>
2020-05-12 11:52:53 +0900

    왼쪽 완성

--- startree/Program.cs
+++ startree/Program.cs
@@ -10,9 +10,13 @@ namespace startree
     {
         static void Main(string[] args)
         {
-            for(int i=0; i<15; i++)
+            for(int i=7; i>0; i--)
             {
-
+                for(int j=0; j<i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
             }
         }
     }
